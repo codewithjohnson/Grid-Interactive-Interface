4f0101c4e1a93425352b3e11c9371c82bb2deb07
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-05-22 14:44:19 +0100

    new

--- index.js
+++ index.js
@@ -1,66 +1,47 @@
 const gridContents = document.querySelector('.content');
 const size = document.querySelector('.size');
-// console.log(size);
 
 const options = document.querySelectorAll('.option');
+
+function clearClassList(){
+    gridContents.classList.remove('auto','max-content','min-content','fifty','hundred','thirty');
+}
 
 options.forEach((e)=>{
     e.addEventListener('click',()=>{
         let value = e.nextElementSibling.textContent;
         if (value === 'auto'){
-            gridContents.classList.remove('max-content');
-            gridContents.classList.remove('min-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('hundred');
-            gridContents.classList.remove('thirty');
+            clearClassList();
             gridContents.classList.toggle(value); 
             size.textContent = value;
         }
         else if (value === 'max-content'){
-            gridContents.classList.remove('auto');
-            gridContents.classList.remove('min-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('hundred');
-            gridContents.classList.remove('thirty');
+            clearClassList();
             gridContents.classList.toggle(value); 
             size.textContent = value;
         }
         else if (value === 'min-content'){
-            gridContents.classList.remove('auto');
-            gridContents.classList.remove('max-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('hundred');
-            gridContents.classList.remove('thirty');
+            clearClassList();
             gridContents.classList.toggle(value); 
             size.textContent = value;
         }
         else if (value === '50px'){
-            gridContents.classList.remove('max-content');
-            gridContents.classList.remove('min-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('hundred');
-            gridContents.classList.remove('thirty');
+            clearClassList();
             gridContents.classList.toggle('fifty'); 
             size.textContent = value;
         }
         else if (value === '100px'){
-            gridContents.classList.remove('auto');
-            gridContents.classList.remove('max-content');
-            gridContents.classList.remove('min-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('thirty');
+            clearClassList();
             gridContents.classList.toggle('hundred'); 
             size.textContent = value;
         }
         else if (value === '30%'){
-            gridContents.classList.remove('auto');
-            gridContents.classList.remove('max-content');
-            gridContents.classList.remove('min-content');
-            gridContents.classList.remove('fifty');
-            gridContents.classList.remove('hundred');
+            clearClassList();
             gridContents.classList.toggle('thirty'); 
             size.textContent = value;
 
         }
     });
 });
+
+
